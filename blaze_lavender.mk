56a565eae2a782956a0a2e66460760c90fd49de5
OmarAlCoptan <omarcoptan9@gmail.com>
2022-06-26 18:52:31 +0200

    Blaze-ify!
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- blaze_lavender.mk
+++ blaze_lavender.mk
@@ -8,17 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Weeb Project stuff
-$(call inherit-product, vendor/weeb/build/product/weeb_product.mk)
+# Inherit some common ProjectBlaze stuff
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+BLAZE_MAINTAINER := COPTAN
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USE_PIXEL_CHARGER := true
+BLAZE_BUILD_TYPE := OFFICIAL
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
-WITH_GMS := true
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := weeb_lavender
+PRODUCT_NAME := blaze_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
